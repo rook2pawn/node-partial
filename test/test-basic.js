527517561295ccebbc94002359cbf0bb45f9bb6a
David Wee <rook2pawn@gmail.com>
2016-04-21 18:07:25 -0700

    added ci

--- test/test-basic.js
+++ test/test-basic.js
@@ -7,7 +7,6 @@ var m3 = function(x,y,z) {
 var foo = partial(m3);
 var baz = partial.rapply(m3);
 
-/*
 // test that you have a function
 test('test the partial is a function', function(t) {
 	t.plan(2);
@@ -15,19 +14,17 @@ test('test the partial is a function', function(t) {
 	t.equal('function',typeof baz);
 });
 // tests basic functionality of partial
-exports.testpartial = function(test) {
-	test.expect(3);
-	test.equal(10,foo(2,3,4));
-	test.equal(10,foo(2,3)(4)); 
-	test.equal(foo(2,3,4),foo(2,3)(4)); 
-	test.done();
-}
+test('test basic functionality of partial', function(t) {
+	t.plan(3);
+	t.equal(foo(2,3,4),10);
+	t.equal(foo(2,3)(4),10);
+	t.equal(foo(2,3,4),foo(2,3)(4));
+});
+
 // tests basic functionality of partial.rapply
-exports.testpartialRight = function(test){
-	test.expect(3);
-	test.equal(10,baz(2,3,4));
-	test.equal(10,baz(4)(2,3));
-	test.equal(baz(2,3,4),baz(4)(2,3)); 
-	test.done();
-}
-*/
+test('test basic functionality of partial right apply', function(t) {
+	t.plan(3);
+	t.equal(baz(2,3,4),10);
+	t.equal(baz(4)(2,3),10);
+	t.equal(baz(2,3,4),baz(4)(2,3));
+})
