2b1c01c18baaf7d341bbad8fba6c6a4be7ea4e7d
Brandon Semilla <semibran@gmail.com>
2017-07-03 14:54:41 -0700

    backwards compatibility

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-var partial = function() {
-  var _args = Array.from(arguments);
-	var fn = _args[0];
-  var args = _args.slice(1);
-	return function() {
-		if (arguments.length + args.length >= fn.length) {
-      var x = args.concat(Array.from(arguments));
-			return fn(...x);
-		} else {	
-      var x = args.concat(Array.from(arguments));
-			return partial(fn,...x)
-		}
-	};
-};
-partial.rapply = function() {
-  var _args = Array.from(arguments);
-	var fn = _args[0];
-  var args = _args.slice(1);
-	return function() {
-		if ((arguments.length + args.length) >= fn.length) {
-      var x = Array.from(arguments).concat(args);
-			return fn(...x);
-		} else {	
-      var x = Array.from(arguments).concat(args);
-			return partial.rapply(fn,...x);
-		}
-	};
-};
-var exports = module.exports = partial;
+var slice = Array.prototype.slice
+
+module.exports = partial
+partial.rapply = rapply
+
+function partial(fn) {
+  var bound = slice.call(arguments, 1)
+  return function () {
+    var args = bound.concat(slice.call(arguments))
+    if (args.length < fn.length) {
+      args.unshift(fn)
+      return partial.apply(this, args)
+    } else {
+      return fn.apply(this, args)
+    }
+  }
+}
+
+function rapply(fn) {
+  var bound = slice.call(arguments, 1)
+  return function () {
+    var args = slice.call(arguments).concat(bound)
+    if (args.length < fn.length) {
+      args.unshift(fn)
+      return rapply.apply(this, args)
+    } else {
+      return fn.apply(this, args)
+    }
+  }
+}
